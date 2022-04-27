9f0cdf2b64f01da700df0ec2466d1d123063e686
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-04-27 11:49:54 -0500

    split into modules, still functioning

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { getFish } from './database.js'
-import { FishList } from './fishList.js'
-
+import { FishList } from './fish.js'
+import { locationHtml } from './locations.js'
 const allFish = getFish()
 
 for (const fish of allFish) {
@@ -8,8 +8,30 @@ for (const fish of allFish) {
 }
 
 
-const parentHTMLElement = document.querySelector(".fish__List")
 
 
-parentHTMLElement.innerHTML = FishList()
+
+
+
+
+const mainContainer = document.querySelector(".main")
+
+
+const applicationHTML = `
+<aside class='care'>
+<h2 class='care__Header'>caring for your fish family</h2>
+<div>  <p class='care__text'>Fishes treat you better than your friends do, and you should treat them with the same love and kindess.  
+    Here are some tips to help keep your finned friends happy and healthy.  It is essential that you keep the water in your fish tank within the appropriate pH levels for your fish. 
+    If the water becomes too alkaline or too acidic it could be fatal to your fish. Before you go about balancing your fish tank, research the ideal pH level for your fish’s species as each breed has different environmental requirements.</p> </div>
+</aside>
+
+<section class="main__Content">
+<section class='fish__List'> ${FishList()} </section>
+
+
+<section class="locations"> ${locationHtml()}
+</section>
+`
+mainContainer.innerHTML = applicationHTML
+
 
